48a512b2d897e2401f5043f1aff5438ed1b6d194
PyoYongChan <tchan95@naver.com>
2018-06-01 15:15:45 +0900

    Mei 05-31
    Created by MeiHaoran

---
+++ TeamProject/TeamProject/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  TeamProject
+//
+//  Created by apple11 on 2018. 5. 30..
+//  Copyright © 2018년 COMP420. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
