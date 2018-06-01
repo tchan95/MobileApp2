0b74b71e37e613f1c6a4e1792166d55fc2caa9b3
meihaoran <meihaoran@naver.com>
2018-06-01 16:37:42 +0900

    0601

---
+++ TeamProject/TeamProject/CalendarView/CalendarViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CalendarViewController.swift
+//  TeamProject
+//
+//  Created by apple11 on 2018. 6. 1..
+//  Copyright © 2018년 COMP420. All rights reserved.
+//
+
+import UIKit
+enum MyTheme {
+    case light
+    case dark
+}
+class CalendarViewController: UIViewController {
+
+    var theme = MyTheme.dark
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "My Calender"
+        self.navigationController?.navigationBar.isTranslucent=false
+        self.view.backgroundColor=Style.bgColor
+        
+        view.addSubview(calenderView)
+        calenderView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10).isActive=true
+        calenderView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -12).isActive=true
+        calenderView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 12).isActive=true
+        calenderView.heightAnchor.constraint(equalToConstant: 365).isActive=true
+        
+        let rightBarBtn = UIBarButtonItem(title: "Light", style: .plain, target: self, action: #selector(rightBarBtnAction))
+        self.navigationItem.rightBarButtonItem = rightBarBtn
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        calenderView.myCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    @objc func rightBarBtnAction(sender: UIBarButtonItem) {
+        if theme == .dark {
+            sender.title = "Dark"
+            theme = .light
+            Style.themeLight()
+        } else {
+            sender.title = "Light"
+            theme = .dark
+            Style.themeDark()
+        }
+        self.view.backgroundColor=Style.bgColor
+        calenderView.changeTheme()
+    }
+    
+    let calenderView: CalenderView = {
+        let v=CalenderView(theme: MyTheme.dark)
+        v.translatesAutoresizingMaskIntoConstraints=false
+        return v
+    }()
+    
+}
